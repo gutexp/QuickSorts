464cf959dc44b5c17300eb8a9a2ca99bebf68a09
gutexp <gutexp@hotmail.com>
2017-08-31 15:07:22 -0300

    Add files via upload

---
+++ QuickSort.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quicksort;
+
+/**
+ *
+ * @author Gustavo Da Cunha Costa Franco Fraga MAT:11218645
+ */
+public class QuickSort {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        int[] arrayOrdena= new int[1000];
+        Testes.iniciaTeste(arrayOrdena);
+        
+    }
+    
+}
